952dd9fe326c78b8ec0a794c1833c8b0eac2b73e
fiskov <fiskov@kru.ru>
2026-07-01 17:01:48 +0700

    feat: create first endpoint

--- apps/blog/apps.py
+++ apps/blog/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class BlogConfig(AppConfig):
-    name = 'blog'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.blog'
+    verbose_name = 'Блог'
