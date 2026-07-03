274033a1220e05c6302b1d0ef92a46459f65fadc
fiskov <fiskov@kru.ru>
2026-07-03 15:41:44 +0700

    feat: create encapsulation of paths

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
+product_patterns = [
+    path("", views.products),
+    path("new/", views.new),
+    path("top/", views.top),
+    path("comment/", views.top),
+    path("question/", views.top),
+]
 
 urlpatterns = [
     path('', views.get_index),
@@ -11,4 +19,6 @@ urlpatterns = [
     path('user/<str:name>/<int:age>/', views.get_user),
     path('user/<str:name>/', views.get_user),
     path('user/', views.get_user),
+    # Подключение инкапсулированных путей
+    path('products/<int:id>/', include(product_patterns)),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -18,3 +18,18 @@ def get_user(request, name:str='Undefined', age:int=0):
             <p>{age}</p>
         </div>
     ''')
+
+def products(request, id):
+    return HttpResponse("<h2>Все товары</h2>")
+
+def new(request):
+    return HttpResponse("<h2>Новые товары</h2>")
+
+def top(request):
+    return HttpResponse("<h2>Популярные товары</h2>")
+
+def comment(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
+
+def question(request, id):
+    return HttpResponse(f"Вопросы о товаре {id}")
