8bca5d66b182e6dc982007bac0a874d2106aab1c
fiskov <fiskov@kru.ru>
2026-07-23 15:49:01 +0700

    feat: add about page

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,4 +3,5 @@ from apps.blog import views
 
 urlpatterns = [
     path('', views.index),
+    path('about/', views.about),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,3 +9,20 @@ def index(request):
 
     data = {"header": header, "langs": langs, "user": user, "is_show_address": is_show_address, "address": address}
     return TemplateResponse(request, "blog/index.html", context=data)
+
+
+def about(request):
+    header = "Наши контакты"
+    contacts = {
+        "buyer": (
+            "Контакты для покупателей", "+7 (3842) 44-03-13"
+        ),
+        "provider": (
+            "Контакты для поставщиков", "+7 (3842) 44-03-09"
+        ),
+        "applicant": (
+            "Контакты для соискателей", "+7 (800) 600-59-99"
+        )}
+
+    data = {"header": header, "contacts": contacts}
+    return TemplateResponse(request, "blog/about.html", context=data)
