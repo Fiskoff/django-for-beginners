4b068365d45f7df7cc806e38acce95aaed122dbc
fiskov <fiskov@kru.ru>
2026-07-23 14:08:26 +0700

    feat: add endpoint

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,24 +1,6 @@
-from django.urls import path, include
-from . import views
-
-
-product_patterns = [
-    path("", views.products),
-    path("new/", views.new),
-    path("top/", views.top),
-    path("comment/", views.top),
-    path("question/", views.top),
-]
+from django.urls import path
+from apps.blog import views
 
 urlpatterns = [
-    path('', views.get_index),
-    # Более конкретный маршрут должен быть объявлен выше, чем более абстрактный, общие маршрут
-    path('about/contact/', views.get_contact),
-    path('about/', views.get_about),
-    # Параметры пути
-    path('user/<str:name>/<int:age>/', views.get_user),
-    path('user/<str:name>/', views.get_user),
-    path('user/', views.get_user),
-    # Подключение инкапсулированных путей
-    path('products/<int:id>/', include(product_patterns)),
+    path('', views.index),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,35 +1,11 @@
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
+def index(request):
+    header = "Данные пользователя"
+    langs = ["Python", "Java", "C#"]
+    user = {"name": "Tom", "age": 23}
+    is_show_address = True
+    address = ("Абрикосовая", 23, 45)
 
-def get_index(request):
-    return HttpResponse("<h2>Главная страница</h2>")
-
-def get_about(request):
-    return HttpResponse("<h2>О сайте</h2>")
-
-def get_contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
-
-def get_user(request, name:str='Undefined', age:int=0):
-    return HttpResponse(f'''
-        <h2>Пользователь</h2>
-        <div>
-            <p>{name}</p>
-            <p>{age}</p>
-        </div>
-    ''')
-
-def products(request, id):
-    return HttpResponse("<h2>Все товары</h2>")
-
-def new(request):
-    return HttpResponse("<h2>Новые товары</h2>")
-
-def top(request):
-    return HttpResponse("<h2>Популярные товары</h2>")
-
-def comment(request, id):
-    return HttpResponse(f"Комментарии о товаре {id}")
-
-def question(request, id):
-    return HttpResponse(f"Вопросы о товаре {id}")
+    data = {"header": header, "langs": langs, "user": user, "is_show_address": is_show_address, "address": address}
+    return TemplateResponse(request, "blog/index.html", context=data)
