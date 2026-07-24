63024507fd77693c262a443f668299009c615b1d
fiskov <fiskov@kru.ru>
2026-07-24 13:20:06 +0700

    fix: add form

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from apps.crud.forms import UserForm
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        return HttpResponse(f'<h2>Привет, {name}, твой возраст: {age}</h2>')
+    else:
+        user_form = UserForm()
+        return render(request, 'crud/index.html', {'form': user_form})
