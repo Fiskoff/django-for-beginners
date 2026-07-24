efb3c6191d01ff12580f17443f4e54c6f8c97746
fiskov <fiskov@kru.ru>
2026-07-24 15:06:11 +0700

    feat: add full CRUD

--- apps/crud/urls.py
+++ apps/crud/urls.py
@@ -3,6 +3,8 @@ from apps.crud import views
 
 urlpatterns = [
     path('create/', views.create, name='create'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete'),
     path('', views.index, name='index'),
 
 ]
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 
 from apps.crud.models import Person
@@ -14,3 +15,24 @@ def create(request):
         person.age = request.POST.get('age')
         person.save()
     return redirect('index')
+
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.age = request.POST.get('age')
+            person.save()
+            return redirect('index')
+        else:
+            return render(request, 'crud/edit_form.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
+
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return redirect('index')
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
