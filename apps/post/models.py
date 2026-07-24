a5c647f4476270f5202d66bcc3208382c6982c8d
fiskov <fiskov@kru.ru>
2026-07-24 10:09:31 +0700

    feat: update model the posts and make migrate

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 class Post(models.Model):
+    # Fields
+    title = models.CharField(max_length=120, default="Без названия")
     text = models.TextField()
 
+    # Metadata
+    # Позволяет установить дополнительные параметры модели, такие как имя таблицы, порядок и подробное имя. Не является обязательным в модели.
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    # Methods
+    # Действуют на экземпляры классов
     def __str__(self):
-        return self.text[:50]
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
